c90e1b57008ac7a9e64cefb99328d5b4e7599389
Gania-Isaro <g.kayumba@alustudent.com>
2026-05-16 20:09:11 +0200

    Add indexes for transactions

--- database/indexes_and_constraints.sql
+++ database/indexes_and_constraints.sql
@@ -3,3 +3,10 @@
 
 USE momo_db;
 
+-- this is index for transaction dates sorting them by the date reported
+CREATE INDEX idx_transactions_date
+    ON transactions (transaction_date);
+
+-- this is for index for filtering transaction by status
+CREATE INDEX idx_transactions_status
+    ON transactions (status); 

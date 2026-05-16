7dd7d7993d9964213e1abb22c660e2ae6de53ae0
Gania-Isaro <g.kayumba@alustudent.com>
2026-05-16 20:32:13 +0200

    add participant and transaction combined indexes

--- database/indexes_and_constraints.sql
+++ database/indexes_and_constraints.sql
@@ -10,3 +10,12 @@ CREATE INDEX idx_transactions_date
 -- this is for index for filtering transaction by status
 CREATE INDEX idx_transactions_status
     ON transactions (status); 
+
+-- to help when filtering by both transaction date and status and for failed transactions in last 7 days 
+CREATE INDEX idx_transactions_date_status
+    ON transactions (status, transaction_date);
+
+-- helps in finding participants faster and alsoo easier
+CREATE INDEX idx_participants_party
+    ON transaction_participants (party_type, party_id);
+
